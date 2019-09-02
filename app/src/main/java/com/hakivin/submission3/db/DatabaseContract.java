4430cfc13c6e33da9d779aa75a161ef295aa7f76
hakivin <hakivinamana@student.uns.ac.id>
2019-09-02 11:15:00 +0700

    add db helper

--- app/src/main/java/com/hakivin/submission3/db/DatabaseContract.java
+++ app/src/main/java/com/hakivin/submission3/db/DatabaseContract.java
@@ -8,9 +8,21 @@ public class DatabaseContract {
 
     static final class MovieColumns implements BaseColumns{
 
+        static String TITLE = "title";
+        static String POSTER = "poster";
+        static String BACKDROP = "backdrop";
+        static String SCORE = "score";
+        static String RELEASE_DATE = "release_date";
+        static String OVERVIEW = "overview";
+
     }
 
     static final class TVColumns implements BaseColumns{
-
+        static String TITLE = "title";
+        static String POSTER = "poster";
+        static String BACKDROP = "backdrop";
+        static String SCORE = "score";
+        static String FIRST_AIR_DATE = "first_air_date";
+        static String OVERVIEW = "overview";
     }
 }
---
+++ app/src/main/java/com/hakivin/submission3/db/DatabaseHelper.java
@@ -0,0 +1,64 @@
+package com.hakivin.submission3.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static String DATABASE_NAME = "dbcatalogueapp";
+
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String SQL_CREATE_TABLE_MOVIE = String.format("CREATE TABLE %s"
+            + " (%s INTEGER PRIMARY KEY," +
+            " %s TEXT NOT NULL," +
+            " %s TEXT NOT NULL," +
+            " %s TEXT NOT NULL," +
+            " %s TEXT NOT NULL," +
+            " %s TEXT NOT NULL," +
+            " %s TEXT NOT NULL)",
+            DatabaseContract.TABLE_MOVIE,
+            DatabaseContract.MovieColumns._ID,
+            DatabaseContract.MovieColumns.TITLE,
+            DatabaseContract.MovieColumns.POSTER,
+            DatabaseContract.MovieColumns.BACKDROP,
+            DatabaseContract.MovieColumns.SCORE,
+            DatabaseContract.MovieColumns.RELEASE_DATE,
+            DatabaseContract.MovieColumns.OVERVIEW
+    );
+
+    private static final String SQL_CREATE_TABLE_TV = String.format("CREATE TABLE %s"
+                    + " (%s INTEGER PRIMARY KEY," +
+                    " %s TEXT NOT NULL," +
+                    " %s TEXT NOT NULL," +
+                    " %s TEXT NOT NULL," +
+                    " %s TEXT NOT NULL," +
+                    " %s TEXT NOT NULL," +
+                    " %s TEXT NOT NULL)",
+            DatabaseContract.TABLE_TV,
+            DatabaseContract.TVColumns._ID,
+            DatabaseContract.TVColumns.TITLE,
+            DatabaseContract.TVColumns.POSTER,
+            DatabaseContract.TVColumns.BACKDROP,
+            DatabaseContract.TVColumns.SCORE,
+            DatabaseContract.TVColumns.FIRST_AIR_DATE,
+            DatabaseContract.TVColumns.OVERVIEW
+    );
+
+    public DatabaseHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_TABLE_MOVIE);
+        db.execSQL(SQL_CREATE_TABLE_TV);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVer, int newVer) {
+        db.execSQL("DROP TABLE IF EXISTS " + DatabaseContract.TABLE_MOVIE);
+        db.execSQL("DROP TABLE IF EXISTS " + DatabaseContract.TABLE_TV);
+        onCreate(db);
+    }
+}
